e238bc6a245e42a3c4a6cb15a3a09275e0a2c7a4
Rodrigo Baggio <rodrigo.baggio.si@gmail.com>
2024-05-03 21:52:27 -0300

    Criacao do projeto

---
+++ src/main/java/com/baggio/lojavirtual/LojavirtualBackendApplication.java
@@ -0,0 +1,13 @@
+package com.baggio.lojavirtual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojavirtualBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojavirtualBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/baggio/lojavirtual/LojavirtualBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baggio.lojavirtual;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojavirtualBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
